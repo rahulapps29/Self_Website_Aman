9376eb532da6344e39cbb06c188328e0f3fab9b8
rahulapps29 <rahulapps29@gmail.com>
2024-11-30 04:40:53 +0530

    second

--- server.js
+++ server.js
@@ -7,28 +7,9 @@ const PORT = 4025;
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Set up a route to render a webpage with a responsive image
+// Default route to serve the HTML file
 app.get("/", (req, res) => {
-  res.send(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Responsive Image</title>
-            <style>
-                img {
-                    max-width: 100%;
-                    height: auto;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>Responsive Image</h1>
-            <img src="/image.jpg" alt="Responsive Example">
-        </body>
-        </html>
-    `);
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
